1c4f5f36dafd1aedb0b243180b7f40b094d3b2eb
mbh1620 <matthaywood2012@gmail.com>
2020-04-25 14:00:39 +0100

    update

--- Computing project/make.bash
+++
@@ -1,3 +0,0 @@
-cd ./Desktop/Computing_Project/Computing\ project
-
-g++ ./main.cpp ./cell_class/cell.cpp ./material_class/material.cpp ./model_class/model.cpp ./vector_class/vector.cpp ./renderer_class/renderer.cpp -o main
--- Computing project/run.bash
+++
@@ -1,5 +0,0 @@
-cd ./Desktop/Computing_Project/Computing\ project
-
-./main
-
-open ./render.html; exit
