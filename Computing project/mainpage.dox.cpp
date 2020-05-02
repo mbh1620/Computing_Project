07835766052736e0f273ed21c9865d278d00cb7b
Matt Haywood <matthaywood2012@gmail.com>
2020-05-02 18:52:26 +0100

    updated doygenmain page

--- Computing project/mainpage.dox.cpp
+++ Computing project/mainpage.dox.cpp
@@ -1,25 +1,10 @@
 /// \file
 
-/*! \mainpage Drawing Shapes
+/*! \mainpage 3D Viewer Documentation
  *
- * This project helps user to draw shapes.
- * Currently two types of shapes can be drawn:
- * - \subpage drawingRectanglePage "How to draw rectangle?"
- *
- * - \subpage drawingCirclePage "How to draw circle?"
- *
+ * This page is for the documentation of the '3DViewer project'
+ * ============================================================
+ * 
+ * 
  */ 
 
-/*! \page drawingRectanglePage How to draw rectangle?
- *
- * Lorem ipsum dolor sit amet
- *
- */
-
-/*! \page drawingCirclePage How to draw circle?
- *
- * This page is about how to draw a circle.
- * Following sections describe circle:
- * - \ref groupCircleDefinition "Definition of Circle"
- * - \ref groupCircleClass "Circle Class"
- */
