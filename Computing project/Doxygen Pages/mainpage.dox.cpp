e1443a795eee0a9b7b772d6b732561a16b5557d8
Matt Haywood <matthaywood2012@gmail.com>
2020-05-03 19:29:47 +0100

    update

--- Computing project/Doxygen Pages/mainpage.dox.cpp
+++ Computing project/Doxygen Pages/mainpage.dox.cpp
@@ -5,7 +5,7 @@
  * This page is for the documentation of the '3DViewer project'
  * ============================================================
  * 
- * The 3D Viewer project is for the second year computing project labs. 
+ * The 3D rendering project has been created for the 2nd Year computing project labs. The task was to create a 3D renderer using VTK, QT and C++. The renderer must load .STL, and .TXT files which are a proprietary file format which our created classes must be able to read in, create the necessary objects and then pass it to VTK to render. 
  * 
 
 
