549204f920db4d4fab5ea859cb031f6adbf52a93
Matt Haywood <matthaywood2012@gmail.com>
2020-05-03 19:03:36 +0100

    update

--- Computing project/Doxygen Pages/mainpage.dox.cpp
+++ Computing project/Doxygen Pages/mainpage.dox.cpp
@@ -1,10 +1,14 @@
 /// \file
 
 /*! \mainpage 3D Viewer Documentation
- *
+ 
  * This page is for the documentation of the '3DViewer project'
  * ============================================================
  * 
+ * The 3D Viewer project is for the second year computing project labs. 
+ * 
+
+
  * Classes:
  *
  *	- Vector 
@@ -14,5 +18,11 @@
  *	- Renderer
  *	- Matrix
  * 	- MainWindow
- * 
- */ 
+
+ * \subpage Instruction_page instructions
+	
+* 
+*/ 
+
+
+
