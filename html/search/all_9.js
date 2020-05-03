1498752c1a30609b587d2f63fc6b6bcc70a02600
Travis CI <travis@travis-ci.com>
2020-05-03 16:59:17 +0000

    Deploy docs: 139
    Commit: 4c24c2f63958875646b9c4d5f08ddab3d3056257

--- html/search/all_9.js
+++ html/search/all_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['id',['id',['../classcell.html#a0fb66db5fe16d551bf2537ef956b3157',1,'cell']]],
+  ['instruction_20page',['Instruction page',['../Instruction.html',1,'']]],
   ['instructions',['Instructions',['../Instruction_page.html',1,'']]],
   ['instructions_2edox',['instructions.dox',['../instructions_8dox.html',1,'']]],
   ['instructions_2edox_2ecpp',['instructions.dox.cpp',['../instructions_8dox_8cpp.html',1,'']]],
--- html/search/pages_2.js
+++ html/search/pages_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['instruction_20page',['Instruction page',['../Instruction.html',1,'']]],
   ['instructions',['Instructions',['../Instruction_page.html',1,'']]]
 ];
