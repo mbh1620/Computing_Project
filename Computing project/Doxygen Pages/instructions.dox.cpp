4c24c2f63958875646b9c4d5f08ddab3d3056257
Matt Haywood <matthaywood2012@gmail.com>
2020-05-03 17:57:36 +0100

    update

--- Computing project/Doxygen Pages/instructions.dox.cpp
+++ Computing project/Doxygen Pages/instructions.dox.cpp
@@ -1,57 +1,57 @@
 /// \file
 
-/*! \page Instruction page
+/*! \page Instruction Instruction page 
  *
  * Instructions for the 3D model Viewer software
  * =============================================
  * 
  * Instructions
 
-* # Adding a .STL file
+* ### Adding a .STL file
 
 * To add a .STL file, click on either the load button in the top left corner or the Add button underneath the model list. A file loader should open. Now navigate to the correct file and choose the file you would like to load. To load a number of models you can simply repeat the process as many times as you wish. 
 
-* # Adding a .txt file (proprietary format)
+* ### Adding a .txt file (proprietary format)
 
 * To add a .txt file repeat the process above however in the file loader, where it says 'files(.stl)', change that to 'All Files ()'. Then open a .txt file.
 
-* # Deleting a model
+* ### Deleting a model
 
 * To delete a model, click on the file in the model list and make sure it is highlighted. Then click the delete button which is located under the file list.
 
-* # Moving/Rotating Models
+* ### Moving/Rotating Models
 
 * To translate a model, first click on the file in the model list. Then type in translation X, Y and Z boxes and the rotation X, Y and Z boxes the amount you would like to translate or rotate the model in the corresponding axes. Now click the 'Move' Button.
 
-* # Changing the colour of a model
+* ### Changing the colour of a model
 
 * Make sure the model is highlighted in the model list. Then go to the 'Model Colour' Drop down list. Click on the colour you wish to change your model to.
 
-* # Changing the Opacity
+* ### Changing the Opacity
 
 * Make sure the model is highlighted in the model list. Then move the opacity slider to the required opacity level.
 
-* # Viewing the model in Wireframe or Solid mode
+* ### Viewing the model in Wireframe or Solid mode
 
 * To change between wireframe and solid, click on the viewer screen and press 'w' for wireframe and 's' for solid.
 
-* # Performing a Cross Section Analysis
+* ### Performing a Cross Section Analysis
 
 * To perform a cross-section analysis check the 'Cross Section Analysis' checkbox and then move the slider left or right depending on where you would like the cross section to be taken.
 
-* # Enabling Centre of Gravity
+* ### Enabling Centre of Gravity
 
 * To see the centre of gravity, simply just check the 'Centre of Gravity' Checkbox.
 
-* # Measuring an object in the viewer
+* ### Measuring an object in the viewer
 
 * To measure an object in the 3d viewer check the 'Measurement Tool' checkbox and then click two points onscreen where you would like to measure between.
 
-* # Changing the Background Colour
+* ### Changing the Background Colour
 
 * To change the background colour click on the BackGround Colour dropdown box and then pick a colour from the list.
 
-* # To see the elements within a proprietary model
+* ### To see the elements within a proprietary model
 
 * You must have a proprietary model loaded. Click on the 'Elements' tab at the top. Then click on the model in the 'Model' list. All of the elements should then appear in the other lists.
 
